4eba424cd478689809ba7814f9293824f46d0f3a
mbayeinsa12 <insam621.com@gmail.com>
2025-07-22 21:34:41 -0700

    non boy c'est quoi ca

--- src/main/java/sn/edu/isepdiamniadio/coursspring/config/SecurityConfig.java
+++ src/main/java/sn/edu/isepdiamniadio/coursspring/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +18,12 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers(HttpMethod.GET,"/api/v1/ecole/").permitAll()
-//                        .requestMatchers(HttpMethod.GET,"/api/v1/ecole/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/home").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/home/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/home/nom/**").permitAll()
-                        .requestMatchers(HttpMethod.GET,"/home/nom/**/age/**").permitAll()
+                       .requestMatchers(HttpMethod.GET,"/api/v1/ecole/").permitAll()
+                       .requestMatchers(HttpMethod.GET,"/api/v1/ecole/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/home/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/home/nom/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/home/nom/**/age/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .logout(Customizer.withDefaults())
@@ -27,5 +31,23 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("kalara")
+                .password("mbaye")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("adminpassword")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails, admin);
     }
 }
